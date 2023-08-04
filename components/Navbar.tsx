d9cd72cfcd7628bca2f6ce85f275c6caa4c76dbc
parteek kumar <digitalparteek@gmail.com>
2023-08-04 15:09:13 +0530

    Added Navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { Menu, Sparkle } from "lucide-react";
+import Link from "next/link";
+import { Poppins } from "next/font/google";
+import { UserButton } from "@clerk/nextjs";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+
+const font = Poppins({
+  weight: "600",
+  subsets: ["latin"],
+});
+
+export const Navbar = () => {
+  return (
+    <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-primary/10 bg-secondary">
+      <div className="flex items-center">
+        <Menu className="block md:hidden" />
+        <Link href="/">
+          <h1
+            className={cn(
+              "hidden md:block text-xl md:text-3xl font-bold text-primary",
+              font.className // dynamic class name
+            )}
+          >
+            genie.ai
+          </h1>
+        </Link>
+      </div>
+      <div className="flex items-center gap-x-3">
+        <Button>
+          Upgrade
+          <Sparkle />
+        </Button>
+        <UserButton />
+      </div>
+    </div>
+  );
+};
