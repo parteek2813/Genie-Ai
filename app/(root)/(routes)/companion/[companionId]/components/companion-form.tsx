8d0f54f6fd9ddcd1583f65108e6d084a1384292e
parteek kumar <digitalparteek@gmail.com>
2023-08-05 10:16:07 +0530

    added companion form page

---
+++ app/(root)/(routes)/companion/[companionId]/components/companion-form.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import React from "react";
+
+export default function CompanionForm() {
+  return <div>companion-form</div>;
+}
--- app/(root)/(routes)/companion/[companionId]/page.tsx
+++ app/(root)/(routes)/companion/[companionId]/page.tsx
@@ -1,5 +1,32 @@
-const CompanionId = () => {
-  return <div>Hello companion Id</div>;
+import { Categories } from "@/components/Categories";
+import prismadb from "@/lib/prismadb";
+import CompanionForm from "./components/companion-form";
+
+interface CompanionIdPageProps {
+  params: {
+    companionId: string;
+  };
+}
+
+const CompanionId = async ({ params }: CompanionIdPageProps) => {
+  // TODO: Check Subscription
+
+  const companion = await prismadb.companion.findUnique({
+    where: {
+      id: params.companionId,
+    },
+  });
+
+  const category = await prismadb.category.findMany({
+    where: {
+      id: params.companionId,
+    },
+  });
+  return (
+    <>
+      <CompanionForm initialData={companion} categories={Categories} />
+    </>
+  );
 };
 
 export default CompanionId;
