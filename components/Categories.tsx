dbf424aa8ce614d10bf3b09727b71b13b0b96275
parteek kumar <digitalparteek@gmail.com>
2023-08-05 09:50:34 +0530

    categories section completed

--- components/Categories.tsx
+++ components/Categories.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+import qs from "query-string";
 import { Category } from "@prisma/client";
+import { useRouter, useSearchParams } from "next/navigation";
 
 import { cn } from "@/lib/utils";
 
@@ -9,32 +11,56 @@ interface CategoryProps {
 }
 
 export const Categories = ({ data }: CategoryProps) => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const categoryId = searchParams.get("categoryId");
+
+  const onClick = (id: string | undefined) => {
+    const query = { categoryId: id };
+
+    const url = qs.stringifyUrl(
+      {
+        url: window.location.href,
+        query,
+      },
+      { skipNull: true }
+    );
+
+    router.push(url);
+  };
+
   return (
     <div className="w-full overflow-x-auto space-x-2 flex p-1">
       <button
-        className={cn(`
-      flex
-      items-center
-      text-center
-      text-xs
-      md:text-sm
-      px-2
-      md:px-4
-      py-2
-      md:py-3
-      rounded-md
-      bg-primary/10
-      hover:opacity-75
-      transition
-      `)}
+        onClick={() => onClick(undefined)}
+        className={cn(
+          ` flex
+            items-center
+            text-center
+            text-xs
+            md:text-sm
+            px-2
+            md:px-4
+            py-2
+            md:py-3
+            rounded-md
+            bg-primary/10
+            hover:opacity-75
+            transition
+      `,
+          !categoryId ? "bg-primary/25" : "bg-primary/10"
+        )}
       >
         Newest
       </button>
 
       {data.map((item) => (
         <button
+          onClick={() => onClick(item.id)}
           key={item.id}
-          className={cn(`
+          className={cn(
+            `
             flex
             items-center
             text-center
@@ -48,7 +74,9 @@ export const Categories = ({ data }: CategoryProps) => {
             bg-primary/10
             hover:opacity-75
             transition
-            `)}
+            `,
+            item.id === categoryId ? "bg-primary/25" : "bg-primary/10"
+          )}
         >
           {item.name}
         </button>
