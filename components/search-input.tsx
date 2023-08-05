e2a08748e74a6e2777fe0493673596b3b767a91b
parteek kumar <digitalparteek@gmail.com>
2023-08-05 08:37:45 +0530

    complete search functionality

--- components/search-input.tsx
+++ components/search-input.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import React, { useState } from "react";
+import qs from "query-string";
+import React, { ChangeEventHandler, useEffect, useState } from "react";
 import { useSearchParams, useRouter } from "next/navigation";
 
 import { Search } from "lucide-react";
@@ -17,10 +18,36 @@ export default function SearchInput() {
   const [value, setValue] = useState(name || "");
   const debouncedValue = useDebounce<string>(value, 500);
 
+  const onChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setValue(e.target.value);
+  };
+
+  useEffect(() => {
+    const query = {
+      name: debouncedValue,
+      categoryId: categoryId,
+    };
+
+    const url = qs.stringifyUrl(
+      {
+        url: window.location.href,
+        query,
+      },
+      { skipEmptyString: true, skipNull: true }
+    );
+
+    router.push(url);
+  }, [debouncedValue, router, categoryId]);
+
   return (
     <div className="relative ">
       <Search className="absolute h-4 w-4 top-3 left-4 to-muted-foreground" />
-      <Input placeholder="Search..." className="pl-10 bg-primary/10 " />
+      <Input
+        onChange={onChange}
+        value={value}
+        placeholder="Search..."
+        className="pl-10 bg-primary/10 "
+      />
     </div>
   );
 }
