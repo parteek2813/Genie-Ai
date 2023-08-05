91d072d3aca811b0606d7400f18b1755e0f3e61a
parteek kumar <digitalparteek@gmail.com>
2023-08-05 12:20:42 +0530

    Companion form complete

--- app/(root)/(routes)/companion/[companionId]/components/companion-form.tsx
+++ app/(root)/(routes)/companion/[companionId]/components/companion-form.tsx
@@ -6,6 +6,15 @@ import React from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { Separator } from "@/components/ui/separator";
+
 interface CompanionFormProps {
   initialData: Companion | null;
   categories: Category[];
@@ -53,5 +62,35 @@ export default function CompanionForm({
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
     console.log(values);
   };
-  return <div className="h-full ">companion-form</div>;
+  return (
+    <div className="h-full p-4 space-y-2 max-w-3xl mx-auto">
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-8 pb-10"
+        >
+          <div className="space-y-2 w-full ">
+            <div>
+              <h3 className="text-lg font-medium">General INformation</h3>
+              <p className="text-sm text-muted-foreground">
+                General Info about ur companion
+              </p>
+            </div>
+
+            <Separator className="bg-primary/10" />
+          </div>
+
+          <FormField
+            name="src"
+            render={({ field }) => (
+              <FormItem className="flex flex-col items-center justify-center space-y-4 ">
+                <FormControl>Image Upload Component</FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </form>
+      </Form>
+    </div>
+  );
 }
