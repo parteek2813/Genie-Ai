1edacc1ef3cdbb86cbc860dcfd929d387aff38ec
parteek kumar <digitalparteek@gmail.com>
2023-08-05 08:28:25 +0530

    added search Ui

--- components/search-input.tsx
+++ components/search-input.tsx
@@ -1,10 +1,22 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
+import { useSearchParams, useRouter } from "next/navigation";
+
 import { Search } from "lucide-react";
 import { Input } from "@/components/ui/input";
+import { useDebounce } from "@/Hooks/use-debounce";
 
 export default function SearchInput() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const categoryId = searchParams.get("categoryId");
+  const name = searchParams.get("name");
+
+  const [value, setValue] = useState(name || "");
+  const debouncedValue = useDebounce<string>(value, 500);
+
   return (
     <div className="relative ">
       <Search className="absolute h-4 w-4 top-3 left-4 to-muted-foreground" />
