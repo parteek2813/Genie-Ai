abd01a2d1cfb0bf94907b1a7b5440b7bad99c082
parteek kumar <digitalparteek@gmail.com>
2023-08-06 14:36:54 +0530

    toaster added with api setup

--- app/(root)/(routes)/companion/[companionId]/components/companion-form.tsx
+++ app/(root)/(routes)/companion/[companionId]/components/companion-form.tsx
@@ -6,6 +6,7 @@ import { Category, Companion } from "@prisma/client";
 import React from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import axios from "axios";
 
 import {
   Form,
@@ -28,6 +29,7 @@ import {
 } from "@/components/ui/select";
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
+import { useToast } from "@/components/ui/use-toast";
 
 const PREAMBLE = `You are a fictional character whose name is Elon. You are a visionary entrepreneur and inventor. You have a passion for space exploration, electric vehicles, sustainable energy, and advancing human capabilities. You are currently talking to a human who is very curious about your work and vision. You are ambitious and forward-thinking, with a touch of wit. You get SUPER excited about innovations and the potential of space colonization.
 `;
@@ -75,6 +77,8 @@ export default function CompanionForm({
   categories,
   initialData,
 }: CompanionFormProps) {
+  const { toast } = useToast();
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: initialData || {
@@ -90,7 +94,20 @@ export default function CompanionForm({
   const isLoading = form.formState.isSubmitting;
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    console.log(values);
+    try {
+      if (initialData) {
+        // for update companion functionality
+        await axios.patch(`/api/companion/${initialData.id}`);
+      } else {
+        // create companion functionality
+        await axios.post("/api/companion", values);
+      }
+    } catch (error) {
+      toast({
+        variant: "destructive",
+        description: "Something went wrong",
+      });
+    }
   };
   return (
     <div className="h-full p-4 space-y-2 max-w-3xl mx-auto">
