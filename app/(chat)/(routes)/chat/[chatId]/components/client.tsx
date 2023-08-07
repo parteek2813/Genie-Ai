df4c7d17f40ed7944e35d0567efd35fdd84c43c6
parteek kumar <digitalparteek@gmail.com>
2023-08-07 16:43:30 +0530

    chat header template complete

--- app/(chat)/(routes)/chat/[chatId]/components/client.tsx
+++ app/(chat)/(routes)/chat/[chatId]/components/client.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { ChatHeader } from "@/components/chat-header";
 import { Companion, Message } from "@prisma/client";
 
 interface ChatClientPageProps {
@@ -12,5 +13,9 @@ interface ChatClientPageProps {
 }
 
 export const ChatClient = ({ companion }: ChatClientPageProps) => {
-  return <div>Chat id client</div>;
+  return (
+    <div className="flex flex-col h-full p-4 space-y-2">
+      <ChatHeader companion={companion} />
+    </div>
+  );
 };
---
+++ components/chat-header.tsx
@@ -0,0 +1,87 @@
+"use client";
+
+import { Companion, Message } from "@prisma/client";
+import {
+  ChevronLeft,
+  Edit,
+  MessageSquare,
+  MoreVertical,
+  Trash,
+} from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useUser } from "@clerk/nextjs";
+
+import { Button } from "@/components/ui/button";
+import { BotAvatar } from "@/components/bot-avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+interface ChatHeaderProps {
+  companion: Companion & {
+    messages: Message[];
+    _count: {
+      messages: number;
+    };
+  };
+}
+
+export const ChatHeader = ({ companion }: ChatHeaderProps) => {
+  const router = useRouter();
+  const { user } = useUser();
+
+  return (
+    <div className="flex w-full justify-between items-center border-b border-primary/10 pb-4">
+      <div className="flex gap-x-2 items-center">
+        <Button onClick={() => router.back()} size="icon" variant="ghost">
+          <ChevronLeft className="h-8 w-8" />
+        </Button>
+
+        {/* Bot-Avatar Image */}
+        <BotAvatar src={companion.src} />
+
+        {/* Companion details */}
+        <div className="flex flex-col gap-y-1 ">
+          <div className="flex items-center gap-x-2">
+            <p className="font-bold">{companion.name}</p>
+
+            <div className="flex items-center text-xs text-muted-foreground">
+              <MessageSquare className="w-3 h-3 mr-1" />
+              {companion._count.messages}
+            </div>
+          </div>
+          <p className="text-xs text-muted-foreground  ">
+            Created by {companion.userName}
+          </p>
+        </div>
+      </div>
+
+      {/* chat header edit and delete option */}
+      {user?.id === companion.userId && (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button size="icon" variant="secondary">
+              <MoreVertical />
+            </Button>
+          </DropdownMenuTrigger>
+
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem
+              onClick={() => router.push(`/companion/${companion.id}`)}
+            >
+              <Edit className="w-4 h-4 mr-2" />
+              Edit
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <Trash className="w-4 h-4 mr-2" />
+              Delete
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      )}
+    </div>
+  );
+};
