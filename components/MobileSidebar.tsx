c837acb79d7113f0a4ec8c71cf75b7654bf5154c
parteek kumar <digitalparteek@gmail.com>
2023-08-09 14:29:32 +0530

    final

--- components/MobileSidebar.tsx
+++ components/MobileSidebar.tsx
@@ -4,14 +4,14 @@ import { Menu } from "lucide-react";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import Sidebar from "@/components/Sidebar";
 
-export default function MobileSidebar() {
+export default function MobileSidebar({ isPro }: { isPro: boolean }) {
   return (
     <Sheet>
       <SheetTrigger className="md:hidden pr-4 ">
         <Menu />
       </SheetTrigger>
       <SheetContent side="left" className="  p-0 bg-secondary pt-10 w-32">
-        <Sidebar />
+        <Sidebar isPro={isPro} />
       </SheetContent>
     </Sheet>
   );
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -32,7 +32,7 @@ export const Navbar = ({ isPro }: NavBarProps) => {
   return (
     <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-primary/10 bg-secondary h-16">
       <div className="flex items-center">
-        <MobileSidebar />
+        <MobileSidebar isPro={isPro} />
         <Link href="/">
           <h1
             className={cn(
