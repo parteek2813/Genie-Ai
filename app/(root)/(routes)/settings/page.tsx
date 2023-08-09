61b2340787b17ee1b33688d9b2e2061d1fb0865d
parteek kumar <digitalparteek@gmail.com>
2023-08-09 17:49:53 +0530

    Feat: Dummy card details

--- app/(root)/(routes)/settings/page.tsx
+++ app/(root)/(routes)/settings/page.tsx
@@ -1,18 +1,57 @@
 import { SubscriptionButton } from "@/components/subscription-button";
 import { checkSubscription } from "@/lib/subscription";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 const SettingsPage = async () => {
   const isPro = await checkSubscription();
 
   return (
-    <div className="h-full p-4 space-y-2 flex flex-col items-center  ml-40% min-h-screen">
+    <div className="h-full p-4 space-y-4 flex flex-col items-center  ml-40% min-h-screen">
       <h3 className="text-lg font-medium ">Settings</h3>
-      <div className="text-muted-foreground text-sm">
+
+      <div className="text-muted-foreground text-sm ">
         {isPro
           ? "You are currently on a Pro plan."
           : "You are currently on a free plan."}
       </div>
+
       <SubscriptionButton isPro={isPro} />
+
+      <Table className=" mt-[368px]">
+        <TableCaption>Dummy Details to get your subscription.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[250px]">Card Number</TableHead>
+            <TableHead>Expiry</TableHead>
+            <TableHead>CVC</TableHead>
+            <TableHead>Name</TableHead>
+            <TableHead>Country</TableHead>
+            <TableHead>Address Line 1 & 2</TableHead>
+            <TableHead>City</TableHead>
+            <TableHead>State</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell className="font-medium">4242-4242-4242-4242</TableCell>
+            <TableCell>05/55</TableCell>
+            <TableCell>555</TableCell>
+            <TableCell>[Any random]</TableCell>
+            <TableCell>United States</TableCell>
+            <TableCell>Address</TableCell>
+            <TableCell>City</TableCell>
+            <TableCell>California</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
     </div>
   );
 };
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -16,6 +16,13 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Label } from "./ui/label";
+import { Input } from "./ui/input";
 
 const font = Poppins({
   weight: "600",
@@ -46,10 +53,104 @@ export const Navbar = ({ isPro }: NavBarProps) => {
       </div>
       <div className="flex items-center gap-x-3">
         {!isPro && (
-          <Button onClick={proModel.onOpen} variant="premium" size="sm">
-            Upgrade
-            <Sparkle className="h-4 w-4 fill-white text-white ml-2" />
-          </Button>
+          <>
+            <Popover>
+              <PopoverTrigger>Get card info</PopoverTrigger>
+              <PopoverContent className="w-80">
+                <div className="space-y-2">
+                  <p className="text-sm text-muted-foreground mb-4">
+                    Fill this info to get Genie pro! <br /> <br /> Or fill out
+                    any random details with same card and expiry details
+                  </p>
+
+                  <div className="grid gap-2">
+                    <div className="grid grid-cols-3 items-center gap-4">
+                      <Label htmlFor="width">Card No</Label>
+                      <Input
+                        disabled={true}
+                        id="width"
+                        defaultValue="4242-4242-4242-4242"
+                        className="col-span-2 h-8"
+                      />
+                    </div>
+                    <div className="grid grid-cols-3 items-center gap-4">
+                      <Label htmlFor="maxWidth">Expiry</Label>
+                      <Input
+                        disabled={true}
+                        id="maxWidth"
+                        defaultValue="05/55"
+                        className="col-span-2 h-8"
+                      />
+                    </div>
+                    <div className="grid grid-cols-3 items-center gap-4">
+                      <Label htmlFor="height">CVC</Label>
+                      <Input
+                        disabled={true}
+                        id="height"
+                        defaultValue="555"
+                        className="col-span-2 h-8"
+                      />
+                    </div>
+                    <div className="grid grid-cols-3 items-center gap-4">
+                      <Label htmlFor="maxHeight">Address </Label>
+                      <Input
+                        disabled={true}
+                        id="maxHeight"
+                        defaultValue="Address"
+                        className="col-span-2 h-8"
+                      />
+                    </div>
+                    <div className="grid grid-cols-3 items-center gap-4">
+                      <Label htmlFor="maxHeight">Country</Label>
+                      <Input
+                        disabled={true}
+                        id="maxHeight"
+                        defaultValue="United States"
+                        className="col-span-2 h-8"
+                      />
+                    </div>
+                    <div className="grid grid-cols-3 items-center gap-4">
+                      <Label htmlFor="maxHeight">City</Label>
+                      <Input
+                        disabled={true}
+                        id="maxHeight"
+                        defaultValue="City"
+                        className="col-span-2 h-8"
+                      />
+                    </div>
+                    <div className="grid grid-cols-3 items-center gap-4">
+                      <Label htmlFor="maxHeight">State</Label>
+                      <Input
+                        disabled={true}
+                        id="maxHeight"
+                        defaultValue="California"
+                        className="col-span-2 h-8"
+                      />
+                    </div>
+                    <div className="grid grid-cols-3 items-center gap-4">
+                      <Label htmlFor="maxHeight">Pincode</Label>
+                      <Input
+                        disabled={true}
+                        id="maxHeight"
+                        defaultValue="123456"
+                        className="col-span-2 h-8"
+                      />
+                    </div>
+                  </div>
+                </div>
+                {/* bottom */}
+              </PopoverContent>
+            </Popover>
+            <Button
+              className="ml-5"
+              onClick={proModel.onOpen}
+              variant="premium"
+              size="sm"
+            >
+              Upgrade
+              <Sparkle className="h-4 w-4 fill-white text-white ml-2" />
+            </Button>
+          </>
         )}
         <UserButton afterSignOutUrl="/" />
         <TooltipProvider>
